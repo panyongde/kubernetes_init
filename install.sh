2aff19c322acd5a2622d9d1f8a2fea20eaa87dfe
panyongde <panyongde@gmail.com>
2018-01-24 14:01:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -77,7 +77,6 @@ case "$1" in
     install_kube_commands
     ;;
   "kubernetes-master")
-    swapoff -a
     sysctl net.bridge.bridge-nf-call-iptables=1
     restart_kubelet
     kubeadm init --config $KUBECONF
